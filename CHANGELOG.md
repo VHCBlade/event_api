697aeb6bc2e0e6812264aa968a0685171eacefcd
Vincent Haron C. Mamutuk <vhcblade@gmail.com>
2023-06-01 18:42:38 +0800

    0.1.0+1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.0+1
+
+- Fixed exports to export all the libraries
+
 ## 0.1.0
 
 - Initial version.
--- lib/event_api.dart
+++ lib/event_api.dart
@@ -1,6 +1,8 @@
-/// Amazing
+/// Holds APIRepository and implementations of a requester interface
 library event_api;
 
+export 'src/fake.dart';
 export 'src/model.dart';
 export 'src/repository.dart';
+export 'src/response.dart';
 export 'src/server.dart';
